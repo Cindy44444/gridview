80d8aab9bb9ad634a42608d3cdab292211ae4b15
Lab2-13 <Lab2-13@LAB2-13>
2025-03-27 18:38:42 -0600

    ftgver45

--- Acciones.cs
+++ Acciones.cs
@@ -10,7 +10,11 @@ namespace gridview
     {
         List<Hijo> listahijo = new List<Hijo>
             {
-            new Hijo("BEN10",13,155,"cafes","secundaria")
+            new Hijo("BEN10",13,155,"cafes","secundaria"),
+            new Hijo("doralaexploradora",7,238,"cafes","primaria"),
+            new Hijo("peppa",3,245,"negros","no estudia"),
+            new Hijo("Thomas",8,168,"negros","no estudia"),
+            new Hijo("burbuja",5,130,"azules","no estudia"),
             };
         public List<Hijo> MostrarHijo()
         {
